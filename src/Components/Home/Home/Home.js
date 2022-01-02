e445a7c3e307a934b68e9787aacf379416e28390
alihaydarphpprogrammer <alihaydar6599@gmail.com>
2022-01-02 10:03:29 +0600

    header footer create

---
+++ src/Components/Home/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../../Shared/Header/Header';
+import Footer from '../Footer/Footer';
+
+const Home = () => {
+    return (
+        <div className='container'>
+            <Header></Header>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Home;
