30c3a692de4723e3eb4a8505d57d2a897cb2013f
Md Ariful Islam <smarifulislami43@gmail.com>
2022-01-04 04:20:16 +0600

    header issue fixed

--- src/Components/Shared/Header/Header.js
+++ src/Components/Shared/Header/Header.js
@@ -1,34 +1,117 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
-// import { HashLink } from 'react-router-hash-link';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
+import useAuth from '../../../Hooks/useAuth';
+import './Header.css';
+import { useSelector } from 'react-redux';
 
 const Header = () => {
-    return (
-        <div>
-            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-                <Container>
+  const { user, LogOUt } = useAuth();
+  const navigate = useNavigate();
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+  const getCartCount = () => {
+    return cartItems.reduce((qty, item) => qty + Number(item.qty), 0);
+  };
 
-                    <Navbar.Brand href="#home" 
-                    className='text-warning'>Electro Cart</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="me-auto">
-                            <Nav.Link href="/aboutus">About Us</Nav.Link>
-                            <Nav.Link href="/offerproducts">Offer Product</Nav.Link>
-                        </Nav>
-                        <Nav>
-                            <Nav.Link href="#deets">All Product</Nav.Link>
-                            <Nav.Link eventKey={2} href="#memes">
-                                Dashboard
-                            </Nav.Link>
-                        </Nav>
-                        
-                    </Navbar.Collapse>
+  return (
+    <div>
+      <Navbar bg='dark' variant='dark' expand='lg'>
+        <Container>
+          <Navbar.Brand>
+            <Link to='/home' className='text-warning'>
+              ElectroCart
+            </Link>
+          </Navbar.Brand>
 
-                </Container>
-            </Navbar>
-        </div>
-    );
+          <Navbar.Toggle
+            aria-controls='responsive-navbar-nav'
+            className='border-0 ms-auto'
+          />
+          <Link
+            to='/cart'
+            className=' ms-lg-5 ms-3 border-0 bg-transparent position-relative me-4'
+          >
+            <i className='fas fa-shopping-cart fs-3 text-white'></i>{' '}
+            <span className='position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger'>
+              {getCartCount()}{' '}
+              <span className='visually-hidden'>cart item</span>
+            </span>
+          </Link>
+          <Navbar.Collapse id='responsive-navbar-nav'>
+            <Nav className='ms-auto d-flex align-items-start align-items-lg-center '>
+              <NavLink
+                to='/home'
+                className={({ isActive }) =>
+                  isActive
+                    ? 'active mt-4 mt-lg-0'
+                    : 'ms-lg-4 mt-4 mt-lg-0 text-white '
+                }
+              >
+                Home
+              </NavLink>
+
+              <NavLink
+                to='/shop'
+                className={({ isActive }) =>
+                  isActive ? 'active' : 'ms-lg-4 mt-4 mt-lg-0 text-white '
+                }
+              >
+                Shop
+              </NavLink>
+              <NavLink
+                to='/offerproducts'
+                className={({ isActive }) =>
+                  isActive ? 'active' : 'ms-lg-4 mt-4 mt-lg-0 text-white '
+                }
+              >
+                Offer
+              </NavLink>
+              <NavLink
+                to='/aboutus'
+                className={({ isActive }) =>
+                  isActive ? 'active' : 'ms-lg-4 mt-4 mt-lg-0 text-white '
+                }
+              >
+                About us
+              </NavLink>
+
+              {
+                <NavLink
+                  to='/dashboard'
+                  className={({ isActive }) =>
+                    isActive ? 'active ' : 'ms-lg-4 mt-4 mt-lg-0 text-white '
+                  }
+                >
+                  Dashboard
+                </NavLink>
+              }
+              {user?.email ? (
+                <div>
+                  <button
+                    className='btn btn-warning ms-lg-4 my-4 my-lg-0'
+                    onClick={() => LogOUt(navigate)}
+                  >
+                    Logout
+                  </button>
+                </div>
+              ) : (
+                <Link
+                  className='btn btn-warning ms-lg-4 my-4 my-lg-0'
+                  to='/login'
+                >
+                  Login
+                </Link>
+              )}
+              <Navbar.Text>
+                <h5 className='ms-2 mt-2'>{user?.displayName}</h5>
+              </Navbar.Text>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
 };
 
 export default Header;
--- src/Hooks/useFirebase.js
+++ src/Hooks/useFirebase.js
@@ -1,147 +1,143 @@
-import { useEffect, useState } from "react";
-import initializeFirebase from "../Firebase/FirebaseInitialize";
-import { getAuth, createUserWithEmailAndPassword, onAuthStateChanged,signOut,signInWithEmailAndPassword,updateProfile,signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from 'react';
+import initializeFirebase from '../Firebase/FirebaseInitialize';
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+  signInWithEmailAndPassword,
+  updateProfile,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
 
+initializeFirebase();
 
+const useFirebase = () => {
+  const [error, setError] = useState('');
+  const [isLoading, setIsLoadng] = useState(true);
+  const [admin, setIsAdmin] = useState(false);
 
+  const [user, setUser] = useState({});
+  const auth = getAuth();
 
+  const googleProvider = new GoogleAuthProvider();
 
+  const GoogleLogin = () => {
+    return signInWithPopup(auth, googleProvider);
+  };
 
-initializeFirebase()
-
-const useFirebase=()=>{
-
-
-  const [error,setError]=useState('')
-  const[isLoading,setIsLoadng]=useState(true)
-  const [admin,setIsAdmin]=useState(false)
-
-    const [user,setUser]=useState({})
-    const auth=getAuth();
-
-    const googleProvider=new GoogleAuthProvider()
-
-
-
-
-      
-     
-    const GoogleLogin=()=>{
-
-      return signInWithPopup(auth,googleProvider)
-    }
-
-    // create User ////
-   const register=(email,password,name)=>{
-       createUserWithEmailAndPassword(auth,email,password).then((userCredential) => {
-        // Signed in 
+  // create User ////
+  const register = (email, password, name) => {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
         const user = userCredential.user;
-        const newUser={email,displayName:name}
-        setUser(newUser)
+        const newUser = { email, displayName: name };
+        setUser(newUser);
 
         // UPdate user In FIrebse //
         updateProfile(auth.currentUser, {
-          displayName: name
-        }).then(() => {
-          // Profile updated!
-          // ...
-        }).catch((error) => {
-          // An error occurred
-          // ...
-        });
-       
-      
+          displayName: name,
+        })
+          .then(() => {
+            // Profile updated!
+            // ...
+          })
+          .catch((error) => {
+            // An error occurred
+            // ...
+          });
+
         // ...
       })
       .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
-        
+
         // ..
-      })
+      });
+  };
 
+  // LogOUt User //
 
-}
-
-// LogOUt User //
-
-const LogOUt=()=>{
-
-    signOut(auth).then(() => {
+  const LogOUt = () => {
+    signOut(auth)
+      .then(() => {
         // Sign-out successful.
-      }).catch((error) => {
-        setError(error.message)
+      })
+      .catch((error) => {
+        setError(error.message);
         // An error happened.
-    
       });
-}
+  };
 
- 
-// Sign IN user ///
-const Login=(email,password)=>{
- 
+  // Sign IN user ///
+  const Login = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
 
-  return  signInWithEmailAndPassword(auth, email, password)
-  
-  
-}
-     
+  useEffect(() => {
+    fetch(`http://localhost:9000/user/admin/${user.email}`)
+      .then((res) => res.json())
+      .then((data) => setIsAdmin(data.admin));
+  }, [user.email]);
 
-useEffect(()=>{
+  // Observer ///
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+        const uid = user.uid;
+        setUser(user);
+        setIsLoadng(false);
+        // ...
+      } else {
+        setUser({});
+        // User is signed out
+        // ...
+      }
+    });
+    return () => unSubscribe;
+  }, []);
 
-  fetch(`http://localhost:9000/user/admin/${user.email}`).then(res=>res.json()).then(data=>setIsAdmin(data.admin))
+  const saveUsertoDb = (email, displayName) => {
+    const user = { email, displayName };
 
-},[user.email])
- 
+    fetch('http://localhost:9000/users', {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(user),
+    });
+  };
+  const saveGoogleUsertoDb = (email, displayName) => {
+    const user = { email, displayName };
 
-// Observer ///
-useEffect(()=>{
-   const unSubscribe= onAuthStateChanged(auth, (user) => {
-        if (user) {
-          // User is signed in, see docs for a list of available properties
-          // https://firebase.google.com/docs/reference/js/firebase.User
-          const uid = user.uid;
-          setUser(user)
-          setIsLoadng(false)
-          // ...
-        } else {
-            setUser({})
-          // User is signed out
-          // ...
-          
-        }
-      });
-      return ()=>unSubscribe;
-},[])
+    fetch('http://localhost:9000/users', {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
 
-
-const saveUsertoDb=(email,displayName)=>{
-
-  const user={email,displayName};
-
-  fetch('http://localhost:9000/users',{
-    method:'POST',
-    headers:{ "content-type": 'application/json'},
-           body:JSON.stringify(user)
-
-  })
-}
-const saveGoogleUsertoDb=(email,displayName)=>{
-
-  const user={email,displayName};
-
-  fetch('http://localhost:9000/users',{
-    method:'POST',
-    headers:{ "content-type": 'application/json'},
-           body:JSON.stringify(user)
-
-  }).then(res=>res.json()).then(data=>console.log(data))
-}
-     
-
-    return {user,register,LogOUt,Login,error,setError,isLoading,setIsLoadng,GoogleLogin,saveUsertoDb,saveGoogleUsertoDb,admin}
-}
-
+  return {
+    user,
+    register,
+    LogOUt,
+    Login,
+    error,
+    setError,
+    isLoading,
+    setIsLoadng,
+    GoogleLogin,
+    saveUsertoDb,
+    saveGoogleUsertoDb,
+    admin,
+  };
+};
 
 export default useFirebase;
