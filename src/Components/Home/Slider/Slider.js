cec276ad661159c60d0d3ee14ea815843dee1362
Md Ariful Islam <smarifulislami43@gmail.com>
2022-01-04 04:38:29 +0600

    slider issue fixed

--- src/Components/Home/Slider/Slider.js
+++ src/Components/Home/Slider/Slider.js
@@ -1,51 +1,71 @@
 import React from 'react';
 import { Carousel } from 'react-bootstrap';
+import slider1 from '../../../img/slider1.jpg';
+import slider2 from '../../../img/slide2.png';
+import slider3 from '../../../img/slide3.png';
+import { Link } from 'react-router-dom';
 
 const Slider = () => {
-    return (
-        <div className=''>
-            <Carousel>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src="https://i.ibb.co/3v6M9FC/b1.png"
-                        alt="First slide"
-                        style={{ height: '400px' }}
-                    />
-                    <Carousel.Caption>
-                        <h1 className='text-dar'>First slide label</h1>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src="https://i.ibb.co/MPtJ1w0/b2.png"
-                        alt="Second slide"
-                        style={{ height: '400px' }}
-                    />
+  return (
+    <div className='mx-auto'>
+      <Carousel  className='carousel'>
+        <Carousel.Item>
+          <img
+            className='d-block w-100'
+            src={slider1}
+            alt='First slide'
+            style={{ height: '600px' }}
+          />
+          <Carousel.Caption>
+            <h1 className='fw-bold text-uppercase'>Get Powerful electrical tools</h1>
+            <p className='text-white-50'>
+              Nulla vitae elit libero, a pharetra augue mollis interdum.
+            </p>
 
-                    <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src="https://i.ibb.co/52WPmqs/b3.png"
-                        alt="Third slide"
-                        style={{ height: '400px' }}
-                    />
+            <Link to='/shop' className='btn btn-warning mb-4'>
+              Shop Now
+            </Link>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className='d-block w-100'
+            src={slider2}
+            alt='Second slide'
+            style={{ height: '600px' }}
+          />
+          <Carousel.Caption>
+            <h1 className='fw-bold text-uppercase'>Get Powerful electrical tools</h1>
+            <p className='text-white-50'>
+              Nulla vitae elit libero, a pharetra augue mollis interdum.
+            </p>
 
-                    <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel>
-        </div>
-    );
+            <Link to='/shop' className='btn btn-warning mb-4'>
+              Shop Now
+            </Link>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className='d-block w-100'
+            src={slider3}
+            alt='Third slide'
+            style={{ height: '600px' }}
+          />
+          <Carousel.Caption>
+            <h1 className='fw-bold text-uppercase'>Get Powerful electrical tools</h1>
+            <p className='text-white-50'>
+              Nulla vitae elit libero, a pharetra augue mollis interdum.
+            </p>
+
+            <Link to='/shop' className='btn btn-warning mb-4'>
+              Shop Now
+            </Link>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
 };
 
 export default Slider;
